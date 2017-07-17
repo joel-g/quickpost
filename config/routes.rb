6f16c73662d2e9b7f9a4a7ede5714e98e8c06af7
joel-g <joelaguerra1@gmail.com>
2017-07-16 19:50:52 -0700

    adds post route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  post 'entries'
+
   resources :entries
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
